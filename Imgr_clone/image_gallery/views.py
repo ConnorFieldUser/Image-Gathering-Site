585940ad77dab979fd5398e5e89c999307544614
ConnorFieldUser <conquest.field@gmail.com>
2016-10-31 14:55:13 -0400

    commit

--- Imgr_clone/image_gallery/views.py
+++ Imgr_clone/image_gallery/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from django.views.generic import TemplateView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 
 # Create your views here.
+
+class IndexView(TemplateView):
+        template_name = 'index.html'
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
